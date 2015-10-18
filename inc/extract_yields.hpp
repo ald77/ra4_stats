b240040193e5cd6bb3f6836b778b0d330659537c
Adam Dishaw <aldishaw@gmail.com>
2015-10-19 01:09:07 +0200

    Add error bars to fit plots

--- inc/extract_yields.hpp
+++ inc/extract_yields.hpp
@@ -1,11 +1,54 @@
 #ifndef H_EXTRACT_YIELDS
 #define H_EXTRACT_YIELDS
 
-struct Yields{
-  double nobs; 
-  double nbkg;
-  double nexp;
-  double nmc;
-};
+#include <string>
+#include <vector>
+#include <limits>
+
+#include "TH1D.h"
+#include "TGraphErrors.h"
+
+#include "RooWorkspace.h"
+#include "RooFitResult.h"
+
+void MakeYieldPlot(RooWorkspace &w,
+		   const RooFitResult &f,
+		   const std::string &file_name);
+
+std::vector<std::string> GetBinNames(const RooWorkspace &w);
+std::vector<std::string> GetProcessNames(const RooWorkspace &w);
+
+std::vector<std::vector<double> > GetComponentYields(const RooWorkspace &w,
+						       const std::vector<std::string> &bin_names,
+						       const std::vector<std::string> &process_names);
+
+std::vector<TH1D> MakeBackgroundHistos(const std::vector<std::vector<double> > &component_yields,
+				       const std::vector<std::string> &bin_names,
+				       const std::vector<std::string> &prc_names);
+
+TH1D MakeTotalHisto(const RooWorkspace &w,
+		    const RooFitResult &f,
+		    const std::vector<std::string> &bin_names);
+
+TH1D MakeObserved(const RooWorkspace &w,
+		  const std::vector<std::string> &bin_names);
+
+void SetBounds(TH1D &a,
+	       TH1D &b,
+	       std::vector<TH1D> &cs);
+
+double GetMaximum(const TH1D &a,
+		  const TH1D &b,
+		  const std::vector<TH1D> &cs);
+
+double GetMinimum(const TH1D &a,
+		  const TH1D &b,
+		  const std::vector<TH1D> &cs);
+
+double GetMaximum(const TH1D &h, double y = std::numeric_limits<double>::max());
+double GetMinimum(const TH1D &h, double y = -std::numeric_limits<double>::max());
+
+TGraphErrors MakeErrorBand(const TH1D &h);
+TGraphErrors MakeRatio(const TH1D &num, const TH1D &den);
 
 #endif
--- src/extract_yields.cxx
+++ src/extract_yields.cxx
@@ -1,22 +1,17 @@
 #include "extract_yields.hpp"
 
 #include <iostream>
+#include <iomanip>
 #include <sstream>
-#include <set>
-#include <map>
-#include <string>
+#include <algorithm>
 
 #include "TFile.h"
-#include "TIterator.h"
 #include "TCanvas.h"
 #include "TPad.h"
-#include "TH1D.h"
-#include "TLine.h"
 #include "TLegend.h"
+#include "TColor.h"
 
-#include "RooWorkspace.h"
-#include "RooArgSet.h"
-#include "RooAbsArg.h"
+#include "RooArgList.h"
 #include "RooRealVar.h"
 
 #include "utilities.hpp"
@@ -24,180 +19,396 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-  if(argc < 2) return 1;
-
+int main(){
   styles style("RA4");
-  string init_file = argv[1];
+  style.setDefaultStyle();
 
-  string command = "combine -M MultiDimFit -H ProfileLikelihood --algo cross --saveWorkspace --floatAllNuisances 1 --robustFit 1 --profilingMode all --floatOtherPOIs 1 "+init_file;
-  cout << command << endl;
-  string output = execute(command);
-  cout << output << endl;
-
-  TFile w_file("higgsCombineTest.MultiDimFit.mH120.root", "read");
+  TFile w_file("higgsCombineTest.MaxLikelihoodFit.mH120.123456.root","read");
   if(!w_file.IsOpen()) return 1;
   RooWorkspace *w = static_cast<RooWorkspace*>(w_file.Get("w"));
   if(w == nullptr) return 1;
-  if(!w->loadSnapshot("MultiDimFit")) return 1;
-  RooRealVar *vr = w->var("r");
-  double r = (vr != nullptr) ? vr->getVal() : -999.;
-  ostringstream r_string;
-  r_string << "r = " << r << flush;
-  RooArgSet args = w->allVars();
-  TIterator *iter_ptr = args.createIterator();
-  if(iter_ptr == nullptr) return 1;
 
-  map<string, Yields> ymap;
-  map<string, double> kmap;
+  TFile fit_file("mlfit.root","read");
+  if(!fit_file.IsOpen()) return 1;
+  RooFitResult *fit_b = static_cast<RooFitResult*>(fit_file.Get("fit_b"));
+  RooFitResult *fit_s = static_cast<RooFitResult*>(fit_file.Get("fit_s"));
 
-  for(; iter_ptr != nullptr && *(*iter_ptr) != nullptr; iter_ptr->Next()){
-    RooAbsArg *var = static_cast<RooAbsArg*>(*(*iter_ptr));
-    if(var == nullptr) continue;
-    string name = var->GetName();
-    if(StartsWith(name, "nobs_BLK_")
-       || StartsWith(name, "nbkg_BLK_")
-       || StartsWith(name, "nexp_BLK_")
-       || StartsWith(name, "ymc_BLK_")
-       || StartsWith(name, "kappamc_BLK_")){
-      string base_name = name.substr(5);
-      if(StartsWith(name, "ymc_BLK_")) base_name = name.substr(4);
-      if(StartsWith(name, "kappamc_BLK_")) base_name = name.substr(8);
+  if(fit_b != nullptr) MakeYieldPlot(*w, *fit_b, "background_fit.pdf");
+  if(fit_s != nullptr) MakeYieldPlot(*w, *fit_s, "signal_fit.pdf");
+}
 
-      RooAbsReal *vnobs = static_cast<RooAbsReal*>(w->arg(("nobs_"+base_name).c_str()));
-      RooAbsReal *vnbkg = static_cast<RooAbsReal*>(w->arg(("nbkg_"+base_name).c_str()));
-      RooAbsReal *vnexp = static_cast<RooAbsReal*>(w->arg(("nexp_"+base_name).c_str()));
-      RooAbsReal *vnmc = static_cast<RooAbsReal*>(w->arg(("ymc_"+base_name).c_str()));
-      RooAbsReal *vkappa = static_cast<RooAbsReal*>(w->arg(("kappamc_"+base_name).c_str()));
-      if(vnobs == nullptr
-	 || vnbkg == nullptr
-	 || vnexp == nullptr
-	 || vnmc == nullptr) continue;
-      Yields yields;
-      yields.nobs = vnobs->getVal();
-      yields.nbkg = vnbkg->getVal();
-      yields.nexp = vnexp->getVal();
-      yields.nmc = vnmc->getVal();
-      ymap[base_name] = yields;
-      kmap[base_name] = (vkappa != nullptr) ? vkappa->getVal() : 1.;
+void MakeYieldPlot(RooWorkspace &w,
+                   const RooFitResult &f,
+                   const string &file_name){
+  bool set_r = false;
+  RooArgList pars = f.floatParsFinal();
+  for(int ipar = 0; ipar < pars.getSize(); ++ipar){
+    RooRealVar *fit_var = static_cast<RooRealVar*>(pars.at(ipar));
+    if(fit_var == nullptr) continue;
+    RooRealVar *w_var = w.var(fit_var->GetName());
+    if(w_var == nullptr) continue;
+    w_var->setVal(fit_var->getVal());
+    if(fit_var->GetName() == string("r")) set_r = true;
+  }
+  RooRealVar *r_var = static_cast<RooRealVar*>(w.var("r"));
+  if(r_var != nullptr){
+    if(!set_r){
+      r_var->setVal(0);
+      r_var->setConstant(true);
+    }else{
+      r_var->setConstant(false);
     }
   }
 
-  TH1D hobs("hobs", ";;Yield", ymap.size(), 0.5, ymap.size()+0.5);
-  TH1D hbkg("hbkg", ";;Yield", ymap.size(), 0.5, ymap.size()+0.5);
-  TH1D hexp("hexp", ";;Yield", ymap.size(), 0.5, ymap.size()+0.5);
-  TH1D hmc("hmc", ";;Yield", ymap.size(), 0.5, ymap.size()+0.5);
-  TH1D hkappa("hkappa", ";;#kappa", ymap.size(), 0.5, ymap.size()+0.5);
+  vector<string> bin_names = GetBinNames(w);
+  vector<string> prc_names = GetProcessNames(w);
 
-  size_t bin = 1;
-  for(auto yield = ymap.cbegin(); yield != ymap.cend(); ++yield){
-    hobs.SetBinContent(bin, yield->second.nobs);
-    hbkg.SetBinContent(bin, yield->second.nbkg);
-    hexp.SetBinContent(bin, yield->second.nexp);
-    hmc.SetBinContent(bin, yield->second.nmc);
+  vector<vector<double> > component_yields = GetComponentYields(w, bin_names, prc_names);
 
-    ++bin;
-  }
-  bin = 1;
-  for(auto kappa = kmap.cbegin(); kappa != kmap.cend(); ++kappa){
-    hkappa.SetBinContent(bin, kappa->second);
-    size_t pos = kappa->first.find("_BIN_");
-    hkappa.GetXaxis()->SetBinLabel(bin, (kappa->first.substr(pos+5)).c_str());
-    ++bin;
-  }
+  vector<TH1D> histos = MakeBackgroundHistos(component_yields, bin_names, prc_names);
+  TH1D signal = MakeTotalHisto(w, f, bin_names);
+  TGraphErrors band = MakeErrorBand(signal);
+  TH1D obs = MakeObserved(w, bin_names);
 
-  double font_size = 0.1;
-  double offset = 0.5;
-
-  hkappa.SetLineColor(kBlue);
-  hkappa.SetLineWidth(6);
-  hkappa.SetLineStyle(1);
-  hkappa.SetFillColor(0);
-  hkappa.SetFillStyle(4000);
-  hkappa.SetStats(0);
-  hkappa.SetLabelSize(font_size, "XY");
-  hkappa.SetTitleSize(font_size, "XY");
-  hkappa.SetTitleOffset(offset, "Y");
-  hkappa.GetXaxis()->LabelsOption("U");
-
-  hobs.SetLineColor(kBlack);
-  hobs.SetLineWidth(6);
-  hobs.SetLineStyle(1);
-  hobs.SetFillColor(0);
-  hobs.SetFillStyle(4000);
-  hobs.SetStats(false);
-  hobs.SetLabelSize(font_size, "Y");
-  hobs.SetTitleSize(font_size, "Y");
-  hobs.SetTitleOffset(offset, "Y");
-
-  hmc.SetLineColor(kBlue);
-  hmc.SetLineWidth(3);
-  hmc.SetLineStyle(2);
-  hmc.SetFillColor(0);
-  hmc.SetFillStyle(4000);
-
-  hbkg.SetLineColor(0);
-  hbkg.SetLineWidth(0);
-  hbkg.SetLineStyle(0);
-  hbkg.SetFillColor(kGreen+2);
-
-  hexp.SetLineColor(0);
-  hexp.SetLineWidth(0);
-  hexp.SetLineStyle(0);
-  hexp.SetFillColor(kRed+2);
+  SetBounds(obs, signal, histos);
 
   TCanvas c;
   c.cd();
   TPad bot_pad("bot_pad", "bot_pad", 0., 0., 1., 0.4);
-  bot_pad.SetFillColor(0);
-  bot_pad.SetFillStyle(4000);
-  bot_pad.SetMargin(0.1, 0., 0.4, 0.);
+  bot_pad.SetFillColor(0); bot_pad.SetFillStyle(4000);
+  bot_pad.SetMargin(0.1, 0., 0.5, 0.);
   bot_pad.Draw();
   c.cd();
-  TPad mid_pad("mid_pad", "mid_pad", 0., 0.4, 1., 0.9);
-  mid_pad.SetFillColor(0);
-  mid_pad.SetFillStyle(4000);
-  mid_pad.SetMargin(0.1, 0., 0., 0.);
+  TPad mid_pad("mid_pad", "mid_pad", 0., 0.4, 1., 0.85);
+  mid_pad.SetFillColor(0); mid_pad.SetFillStyle(4000);
+  mid_pad.SetMargin(0.1, 0., 0.0, 0.);
   mid_pad.Draw();
   c.cd();
-  TPad top_pad("top_pad", "top_pad", 0., 0.9, 1., 1.);
-  top_pad.SetFillColor(0);
-  top_pad.SetFillStyle(4000);
-  top_pad.SetMargin(0.1, 0., 0., 0.);
+  TPad top_pad("top_pad", "top_pad", 0., 0.85, 1., 1.0);
+  top_pad.SetFillColor(0); top_pad.SetFillStyle(4000);
+  top_pad.SetMargin(0.1, 0., 0.0, 0.);
   top_pad.Draw();
+
+  double font_size = 0.1;
+  double offset = 0.5;
 
   mid_pad.cd();
   mid_pad.SetLogy();
-  hobs.Draw();
-  hexp.Draw("same");
-  hbkg.Draw("same");
-  hobs.Draw("same");
-  hmc.Draw("same");
+  signal.SetTitleSize(font_size, "Y");
+  signal.SetTitleOffset(offset, "Y");
+  signal.Draw();
+  for(auto h = histos.rbegin(); h!= histos.rend(); ++h){
+    h->Draw("same");
+  }
+  band.Draw("02 same");
+  obs.Draw("e1p same");
 
   top_pad.cd();
   TLegend l(0.1, 0., 1., 1.);
   l.SetNColumns(3);
+  l.SetFillColor(0); l.SetFillStyle(4000);
   l.SetBorderSize(0);
-  l.SetFillColor(0);
-  l.SetFillStyle(4000);
-  l.AddEntry(&hobs, "Observed", "l");
-  l.AddEntry(&hbkg, "Background Fit", "f");
-  l.AddEntry(&hobs, r_string.str().c_str(), "");
-  l.AddEntry(&hmc, "MC Yield", "l");
-  l.AddEntry(&hexp, "Signal Fit", "f");
+  l.AddEntry(&obs, "Observed", "lep");
+  l.AddEntry(&signal, "Signal", "f");
+  ostringstream oss;
+  oss << setprecision(2) << fixed;
+  oss << "r=";
+  if(r_var == nullptr){
+    oss << "???";
+  }else if(r_var->isConstant()){
+    oss << r_var->getVal() << " (fixed)";
+  }else{
+    oss << r_var->getVal() << "#pm" << r_var->getPropagatedError(f);
+  }
+  oss << flush;
+  l.AddEntry(&obs, oss.str().c_str(), "");
+  for(auto h = histos.crbegin(); h != histos.crend(); ++h){
+    l.AddEntry(&(*h), h->GetName(), "f");
+  }
   l.Draw("same");
 
   bot_pad.cd();
-  hkappa.Draw();
-  TLine line(0.5, 1., kmap.size()+0.5, 1.);
-  line.SetLineColor(kBlack);
-  line.SetLineStyle(2);
-  line.SetLineWidth(1);
-  line.Draw("same");
+  TGraphErrors obs_rat = MakeRatio(obs, signal);
+  TGraphErrors pred_rat = MakeRatio(signal, signal);
+  TH1D dumb = obs;
+  dumb.SetLineColor(0);
+  dumb.SetLineWidth(0);
+  dumb.SetFillColor(0);
+  dumb.SetFillStyle(4000);
+  dumb.SetMinimum(0.);
+  dumb.SetMaximum(2.);
+  dumb.SetTitle(";;Obs/Pred ");
+  dumb.GetXaxis()->LabelsOption("V");
+  dumb.SetTitleSize(font_size, "Y");
+  dumb.SetTitleOffset(offset, "Y");
+  dumb.Draw();
+  pred_rat.SetFillColor(kGray);
+  pred_rat.SetFillStyle(3001);
+  pred_rat.Draw("02 same");
+  obs_rat.Draw("0 same");
+  c.Print(file_name.c_str());
+}
 
-  c.Print((init_file+"_yields.pdf").c_str());
+vector<string> GetBinNames(const RooWorkspace &w){
+  vector<string> names;
+  TIter iter(w.allFunctions().createIterator());
+  int size = w.allFunctions().getSize();
+  TObject *obj;
+  int i = 0;
+  while((obj = iter()) && i < size){
+    ++i;
+    RooAbsArg *arg = static_cast<RooAbsArg*>(obj);
+    if(arg == nullptr) continue;
+    string name = arg->GetName();
+    if(name.substr(0,9) != "nexp_BLK_") continue;
+    string bin_name = name.substr(5);
+    Append(names, bin_name);
+  }
+  iter.Reset();
+  reverse(names.begin(), names.end());
+  return names;
+}
 
-  iter_ptr->Reset();
-  delete iter_ptr;
-  iter_ptr = nullptr;
+vector<string> GetProcessNames(const RooWorkspace &w){
+  vector<string> names;
+  TIter iter(w.allFunctions().createIterator());
+  int size = w.allFunctions().getSize();
+  TObject *obj;
+  int i = 0;
+  while((obj = iter()) && i < size){
+    ++i;
+    RooAbsArg *arg = static_cast<RooAbsArg*>(obj);
+    if(arg == nullptr) continue;
+    string name = arg->GetName();
+    if(name.substr(0,9) != "frac_BIN_") continue;
+    auto prc_pos = name.find("_PRC_");
+    if(prc_pos == string::npos) continue;
+    string bin_name = name.substr(prc_pos+5);
+    if(find(names.cbegin(), names.cend(), bin_name) != names.cend()) continue;
+    Append(names, bin_name);
+  }
+  iter.Reset();
+  return names;
+}
+
+vector<vector<double> > GetComponentYields(const RooWorkspace &w,
+                                           const vector<string> &bin_names,
+                                           const vector<string> &prc_names){
+  vector<vector<double> > yields(bin_names.size());
+  for(auto &bin: yields){
+    bin = vector<double>(prc_names.size(), 0.);
+  }
+  for(size_t ibin = 0; ibin < yields.size(); ++ibin){
+    const string &bin_name = bin_names.at(ibin);
+    auto blk_pos = bin_name.find("_BIN_");
+    if(blk_pos == string::npos) continue;
+    string plain_name = bin_name.substr(blk_pos+5);
+    for(size_t iprc = 0; iprc < yields.at(ibin).size(); ++iprc){
+      const string &prc_name = prc_names.at(iprc);
+      RooRealVar *nbkg_arg = static_cast<RooRealVar*>(w.function(("nbkg_"+bin_name).c_str()));
+      if(nbkg_arg == nullptr) continue;
+      RooRealVar *frac_arg = static_cast<RooRealVar*>(w.function(("frac_BIN_"+plain_name+"_PRC_"+prc_name).c_str()));
+      if(frac_arg == nullptr) continue;
+      yields.at(ibin).at(iprc) = nbkg_arg->getVal() * frac_arg->getVal();
+    }
+  }
+  return yields;
+}
+
+vector<TH1D> MakeBackgroundHistos(const vector<vector<double> > &yields,
+                                  const vector<string> &bin_names,
+                                  const vector<string> &prc_names){
+  if(yields.size() == 0){
+    return vector<TH1D>();
+  }
+  vector<TH1D> histos(yields.at(0).size(),
+                      TH1D("", ";;Yield ", yields.size(), 0.5, yields.size()+0.5));
+  for(size_t ibin = 0; ibin < yields.size(); ++ibin){
+    for(size_t iprc = 0; iprc < yields.at(ibin).size(); ++iprc){
+      histos.at(iprc).SetBinContent(ibin+1, yields.at(ibin).at(iprc));
+    }
+  }
+
+  for(size_t iprc = 0; iprc < histos.size(); ++iprc){
+    TH1D &h = histos.at(iprc);
+    h.SetName(prc_names.at(iprc).c_str());
+    h.SetFillColor(iprc+3);
+    h.SetLineColor(iprc+3);
+    h.SetLineWidth(0);
+    for(size_t ibin = 0; ibin < bin_names.size(); ++ibin){
+      const string &name = bin_names.at(ibin);
+      auto pos = name.find("_BIN_");
+      if(pos == string::npos) continue;
+      string label = name.substr(pos+5);
+      h.GetXaxis()->SetBinLabel(ibin+1, label.c_str());
+    }
+  }
+  sort(histos.begin(), histos.end(),
+       [](const TH1D &a, const TH1D &b) -> bool{return a.Integral() < b.Integral();});
+
+  for(size_t iprc = histos.size()-1; iprc < histos.size(); --iprc){
+    TH1D &h = histos.at(iprc);
+    for(size_t isum = iprc-1; isum < histos.size(); --isum){
+      h.Add(&histos.at(isum));
+    }
+  }
+
+  return histos;
+}
+
+TH1D MakeTotalHisto(const RooWorkspace &w,
+                    const RooFitResult &f,
+                    const vector<string> &bin_names){
+  TH1D h("signal", ";;Yield ", bin_names.size(), 0.5, bin_names.size()+0.5);
+  h.SetFillColor(2);
+  h.SetLineColor(2);
+  h.SetLineWidth(0);
+
+  for(size_t ibin = 0; ibin < bin_names.size(); ++ibin){
+    const string &name = bin_names.at(ibin);
+    auto pos = name.find("_BIN_");
+    if(pos == string::npos) continue;
+    string label = name.substr(pos+5);
+    h.GetXaxis()->SetBinLabel(ibin+1, label.c_str());
+    RooRealVar *var = static_cast<RooRealVar*>(w.function(("nexp_"+name).c_str()));
+    if(var == nullptr) continue;
+    h.SetBinContent(ibin+1, var->getVal());
+    h.SetBinError(ibin+1, var->getPropagatedError(f));
+  }
+
+  return h;
+}
+
+TH1D MakeObserved(const RooWorkspace &w,
+                  const vector<string> &bin_names){
+  TH1D h("observed", ";;Yield ", bin_names.size(), 0.5, bin_names.size()+0.5);
+  h.SetLineColor(1);
+  h.SetFillColor(0);
+  h.SetFillStyle(4000);
+
+  for(size_t ibin = 0; ibin < bin_names.size(); ++ibin){
+    const string &name = bin_names.at(ibin);
+    auto pos = name.find("_BIN_");
+    if(pos == string::npos) continue;
+    string label = name.substr(pos+5);
+    h.GetXaxis()->SetBinLabel(ibin+1, label.c_str());
+    RooRealVar *var = static_cast<RooRealVar*>(w.var(("nobs_"+name).c_str()));
+    if(var == nullptr) continue;
+    h.SetBinContent(ibin+1, var->getVal());
+  }
+
+  return h;
+}
+
+void SetBounds(TH1D &a,
+	       TH1D &b,
+	       std::vector<TH1D> &cs){
+  double factor = 0.02;
+
+  double hmax = GetMaximum(a, b, cs);
+  double hmin = GetMinimum(a, b, cs);
+  double lmax = log(hmax);
+  double lmin = log(hmin);
+  double log_diff = lmax-lmin;
+  lmin -= factor*log_diff;
+  lmax += factor*log_diff;
+  hmin = exp(lmin);
+  hmax = exp(lmax);
+  a.SetMinimum(hmin);
+  a.SetMaximum(hmax);
+  b.SetMinimum(hmin);
+  b.SetMaximum(hmax);
+  for(auto &c: cs){
+    c.SetMinimum(hmin);
+    c.SetMaximum(hmax);
+  }
+}
+
+double GetMaximum(const TH1D &a,
+		  const TH1D &b,
+		  const vector<TH1D> &cs){
+  double the_max = GetMaximum(a);
+  double this_max = GetMaximum(b);
+  if(this_max > the_max) the_max = this_max;
+  for(const auto &c: cs){
+    this_max = GetMaximum(c);
+    if(this_max > the_max) the_max = this_max;
+  }
+  return the_max;
+}
+
+double GetMinimum(const TH1D &a,
+		  const TH1D &b,
+		  const vector<TH1D> &cs){
+  double the_min = GetMinimum(a, 0.1);
+  double this_min = GetMinimum(b, 0.1);
+  if(this_min < the_min) the_min = this_min;
+  for(const auto &c: cs){
+    this_min = GetMinimum(c, 0.1);
+    if(this_min < the_min) the_min = this_min;
+  }
+  return the_min;
+}
+
+double GetMaximum(const TH1D &h, double y){
+  double the_max = -numeric_limits<double>::max();
+  for(int bin = 1; bin <= h.GetNbinsX(); ++bin){
+    double content = h.GetBinContent(bin);
+    if(content > the_max){
+      if(content < y){
+	the_max = content;
+      }else{
+	the_max = y;
+      }
+    }
+  }
+  return the_max;
+}
+
+double GetMinimum(const TH1D &h, double y){
+  double the_min = numeric_limits<double>::max();
+  for(int bin = 1; bin <= h.GetNbinsX(); ++bin){
+    double content = h.GetBinContent(bin);
+    if(content < the_min){
+      if(content > y){
+	the_min = content;
+      }else{
+	the_min = y;
+      }
+    }
+  }
+  return the_min;
+}
+
+TGraphErrors MakeErrorBand(const TH1D &h){
+  TGraphErrors g(h.GetNbinsX());
+  for(int bin = 1; bin <= h.GetNbinsX(); ++bin){
+    g.SetPoint(bin, h.GetBinCenter(bin), h.GetBinContent(bin));
+    g.SetPointError(bin, 0.5, h.GetBinError(bin));
+  }
+  g.SetFillColor(kGray);
+  g.SetFillStyle(3001);
+  return g;
+}
+
+TGraphErrors MakeRatio(const TH1D &num, const TH1D &den){
+  TGraphErrors g(num.GetNbinsX());
+  for(int bin = 1; bin <= num.GetNbinsX(); ++bin){
+    double x = num.GetBinCenter(bin);
+    double nc = num.GetBinContent(bin);
+    double dc = den.GetBinContent(bin);
+    double ne = num.GetBinError(bin);
+    double big_num = 0.5*numeric_limits<float>::max();
+    if(dc != 0.){
+      g.SetPoint(bin, x, nc/dc);
+      g.SetPointError(bin, 0.5, ne/dc);
+    }else if(nc == 0.){
+      g.SetPoint(bin, x, 1.);
+      g.SetPointError(bin, 0.5, big_num);
+    }else{
+      g.SetPoint(bin, x, nc > 0. ? big_num : -big_num);
+      g.SetPointError(bin, 0.5, big_num);
+    }
+  }
+  return g;
 }
