70ac9e07b18b78cfe794056fd9986e8880fb7b17
Adam Dishaw <aldishaw@gmail.com>
2016-05-18 21:15:06 -0700

    Ensure combine results are printed in run_combine.sh

--- run/run_combine.sh
+++ run/run_combine.sh
@@ -10,10 +10,10 @@ fi
 tempfile=temp.log
 
 printf '\n'$rootfile'\n\n' 
-combine -M ProfileLikelihood --significance --expectSignal=1 $rootfile > $tempfile
+combine -v 9999 -M ProfileLikelihood --significance --expectSignal=1 $rootfile > $tempfile
 echo Observed `grep --color=always Significance $tempfile`
 
-combine -M Asymptotic $rootfile > $tempfile
+combine -v 9999 -M Asymptotic $rootfile > $tempfile
 grep --color=always "Observed Limit" $tempfile
 echo A posteriori `grep --color=always "Expected 16.0%" $tempfile`
 echo A posteriori `grep --color=always "Expected 50.0%" $tempfile`
@@ -22,16 +22,16 @@ echo
 
 if (( "$#" > 1 ))
 then
-	combine -M ProfileLikelihood --significance --expectSignal=1  --toysFreq $rootfile > $tempfile
+	combine -v 9999 -M ProfileLikelihood --significance --expectSignal=1  --toysFreq $rootfile > $tempfile
 	echo A posteriori expected `grep --color=always Significance $tempfile`
 	
-	combine -M ProfileLikelihood --significance --expectSignal=1 -t -1  --toysFreq $rootfile > $tempfile
+	combine -v 9999 -M ProfileLikelihood --significance --expectSignal=1 -t -1  --toysFreq $rootfile > $tempfile
 	echo "A posteriori expected (with -t -1)" `grep --color=always Significance $tempfile`
 	
-	combine -M ProfileLikelihood --significance --expectSignal=1 -t -1 $rootfile > $tempfile
+	combine -v 9999 -M ProfileLikelihood --significance --expectSignal=1 -t -1 $rootfile > $tempfile
 	echo A priori expected `grep --color=always Significance $tempfile`
 	
-	combine -M Asymptotic -t -1  $rootfile > $tempfile
+	combine -v 9999 -M Asymptotic -t -1  $rootfile > $tempfile
 	echo A priori `grep --color=always "Expected 16.0%" $tempfile`
 	echo A priori `grep --color=always "Expected 50.0%" $tempfile`
 	echo A priori `grep --color=always "Expected 84.0%" $tempfile`
