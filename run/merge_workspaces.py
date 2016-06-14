2bd9b2bbe1c4f9a49c11c4f412b8c669ab9950ca
Adam Dishaw <aldishaw@gmail.com>
2016-06-14 14:06:08 -0700

    Simplify arguments and allow more than two workspaces to be merged

--- run/merge_workspaces.py
+++ run/merge_workspaces.py
@@ -3,25 +3,20 @@
 import argparse
 import ROOT
 
-def ImportPdfs(wspace_out, wspace_in, suffix, shared_vars):
-    getattr(wspace_out, "import")(wspace_in.allPdfs(),
-                                  ROOT.RooFit.RenameAllVariablesExcept(suffix, ",".join(shared_vars)),
-                                  ROOT.RooFit.RenameAllNodes(suffix))
+def GetLabel(i):
+    return str(2015+i)
 
-def ImportSet(wspace_out, wspace_2016, wspace_2015, set_name, shared_vars):
-    wspace_out.defineSet(set_name, "")
-    ExtendSet(wspace_out, wspace_2016, set_name, shared_vars, "2016")
-    ExtendSet(wspace_out, wspace_2015, set_name, shared_vars, "2015")
-
-def ExtendSet(wspace_out, wspace_in, set_name, shared_vars, suffix):
+def ExtendSet(wspace_out, wspace_in, set_name, suffix, shared_vars):
     iterator = wspace_in.set(set_name).createIterator()
     var = iterator.Next()
     while var:
-        if var.GetName() in shared_vars:
-            wspace_out.extendSet(set_name, var.GetName())
-        else:
-            wspace_out.extendSet(set_name, var.GetName()+"_"+suffix)
+        extension = "" if var.GetName() in shared_vars else "_"+suffix
+        wspace_out.extendSet(set_name, var.GetName()+extension)
         var = iterator.Next()
+
+def AddLikelihood(wspace_out, name, num_merged):
+    likelihood_list = name+"_"+(","+name+"_").join([GetLabel(i) for i in xrange(0, num_merged)])
+    wspace_out.factory("PROD::"+name+"("+likelihood_list+")")
 
 def AddModel(wspace_out, config_name, model_name):
     model_config = ROOT.RooStats.ModelConfig(config_name, wspace_out)
@@ -33,33 +28,36 @@ def AddModel(wspace_out, config_name, model_name):
     getattr(wspace_out, "import")(model_config)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Merges 2015 and 2016 workspaces")
-    parser.add_argument("--file_2016", required=True, help="File containing 2016 workspace")
-    parser.add_argument("--file_2015", required=True, help="File containing 2015 workspace")
-    parser.add_argument("--file_out", required=True, help="File in which to save merged workspace")
-    parser.add_argument("--wspace_2016", default="w", help="Name of 2016 workspace")
-    parser.add_argument("--wspace_2015", default="w", help="Name of 2015 workspace")
+    parser = argparse.ArgumentParser(description="Merges multiple workspaces",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("file_out", help="File in which to save merged workspace")
+    parser.add_argument("file_in", nargs="+", help="File(s) containing workspace(s) to be merged. Typically 1 each for 2015 and 2016.")
     parser.add_argument("--wspace_out", default="w", help="Name of output workspace")
-    parser.add_argument("-s", "--shared", nargs="*", default=["r","dilep_closure"], help="Variables shared or correlated between 2015 and 2016")
+    parser.add_argument("--wspace_in", default="w", help="Name of input workspaces")
+    parser.add_argument("-s", "--shared", nargs="*", default=["r","dilep_closure"], help="Variables shared or correlated across input workspaces")
     args = parser.parse_args()
 
-    file_2016 = ROOT.TFile(args.file_2016, "read")
-    file_2015 = ROOT.TFile(args.file_2015, "read")
-
-    wspace_2016 = file_2016.Get(args.wspace_2016)
-    wspace_2015 = file_2015.Get(args.wspace_2015)
     wspace_out = ROOT.RooWorkspace(args.wspace_out)
 
-    ImportPdfs(wspace_out, wspace_2016, "2016", args.shared)
-    ImportPdfs(wspace_out, wspace_2015, "2015", args.shared)
+    var_sets = ["POI", "nuisances", "observables", "globalObservables"]
+    for var_set in var_sets:
+        wspace_out.defineSet(var_set, "")
 
-    wspace_out.factory("PROD::model_b(model_b_2016, model_b_2015)")
-    wspace_out.factory("PROD::model_s(model_s_2016, model_s_2015)")
+    for ifile in xrange(0, len(args.file_in)):
+        label = GetLabel(ifile)
+        file_in = ROOT.TFile(args.file_in[ifile], "read")
+        wspace_in = file_in.Get(args.wspace_in)
 
-    ImportSet(wspace_out, wspace_2016, wspace_2015, "POI", args.shared)
-    ImportSet(wspace_out, wspace_2016, wspace_2015, "nuisances", args.shared)
-    ImportSet(wspace_out, wspace_2016, wspace_2015, "observables", args.shared)
-    ImportSet(wspace_out, wspace_2016, wspace_2015, "globalObservables", args.shared)
+        getattr(wspace_out, "import")(wspace_in.allPdfs(),
+                                      ROOT.RooFit.RenameAllVariablesExcept(label, ",".join(args.shared)),
+                                      ROOT.RooFit.RenameAllNodes(label))
+        for var_set in var_sets:
+            ExtendSet(wspace_out, wspace_in, var_set, label, args.shared)
+
+        file_in.Close()
+
+    AddLikelihood(wspace_out, "model_b", len(args.file_in))
+    AddLikelihood(wspace_out, "model_s", len(args.file_in))
 
     data_obs = ROOT.RooDataSet("data_obs", "data_obs", wspace_out.set("observables"))
     data_obs.add(wspace_out.set("observables"))
