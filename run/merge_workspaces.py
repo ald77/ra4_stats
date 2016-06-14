61a15e39216f0698ddbbd8002610bfbd71b89b09
Adam Dishaw <aldishaw@gmail.com>
2016-06-14 14:23:55 -0700

    Make dedicated function for merging workspaces

--- run/merge_workspaces.py
+++ run/merge_workspaces.py
@@ -27,6 +27,38 @@ def AddModel(wspace_out, config_name, model_name):
     model_config.SetGlobalObservables(wspace_out.set("globalObservables"))
     getattr(wspace_out, "import")(model_config)
 
+def MergeWorkspaces(files_in, wspace_name_in, file_out, wspace_name_out, shared_vars):
+    wspace_out = ROOT.RooWorkspace(wspace_name_out)
+
+    var_sets = ["POI", "nuisances", "observables", "globalObservables"]
+    for var_set in var_sets:
+        wspace_out.defineSet(var_set, "")
+
+    for ifile in xrange(0, len(files_in)):
+        label = GetLabel(ifile)
+        file_in = ROOT.TFile(files_in[ifile], "read")
+        wspace_in = file_in.Get(wspace_name_in)
+
+        getattr(wspace_out, "import")(wspace_in.allPdfs(),
+                                      ROOT.RooFit.RenameAllVariablesExcept(label, ",".join(shared_vars)),
+                                      ROOT.RooFit.RenameAllNodes(label))
+        for var_set in var_sets:
+            ExtendSet(wspace_out, wspace_in, var_set, label, shared_vars)
+
+        file_in.Close()
+
+    AddLikelihood(wspace_out, "model_b", len(files_in))
+    AddLikelihood(wspace_out, "model_s", len(files_in))
+
+    data_obs = ROOT.RooDataSet("data_obs", "data_obs", wspace_out.set("observables"))
+    data_obs.add(wspace_out.set("observables"))
+    getattr(wspace_out, "import")(data_obs, ROOT.RooCmdArg())
+
+    AddModel(wspace_out, "ModelConfig", "model_s")
+    AddModel(wspace_out, "ModelConfig_bonly", "model_b")
+
+    wspace_out.writeToFile(file_out)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Merges multiple workspaces",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -37,33 +69,4 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--shared", nargs="*", default=["r","dilep_closure"], help="Variables shared or correlated across input workspaces")
     args = parser.parse_args()
 
-    wspace_out = ROOT.RooWorkspace(args.wspace_out)
-
-    var_sets = ["POI", "nuisances", "observables", "globalObservables"]
-    for var_set in var_sets:
-        wspace_out.defineSet(var_set, "")
-
-    for ifile in xrange(0, len(args.file_in)):
-        label = GetLabel(ifile)
-        file_in = ROOT.TFile(args.file_in[ifile], "read")
-        wspace_in = file_in.Get(args.wspace_in)
-
-        getattr(wspace_out, "import")(wspace_in.allPdfs(),
-                                      ROOT.RooFit.RenameAllVariablesExcept(label, ",".join(args.shared)),
-                                      ROOT.RooFit.RenameAllNodes(label))
-        for var_set in var_sets:
-            ExtendSet(wspace_out, wspace_in, var_set, label, args.shared)
-
-        file_in.Close()
-
-    AddLikelihood(wspace_out, "model_b", len(args.file_in))
-    AddLikelihood(wspace_out, "model_s", len(args.file_in))
-
-    data_obs = ROOT.RooDataSet("data_obs", "data_obs", wspace_out.set("observables"))
-    data_obs.add(wspace_out.set("observables"))
-    getattr(wspace_out, "import")(data_obs, ROOT.RooCmdArg())
-
-    AddModel(wspace_out, "ModelConfig", "model_s")
-    AddModel(wspace_out, "ModelConfig_bonly", "model_b")
-
-    wspace_out.writeToFile(args.file_out)
+    MergeWorkspaces(args.file_in, args.wspace_in, args.file_out, args.wspace_out, args.shared)
