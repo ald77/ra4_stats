64484e4bdf5c16a9305aa8adc823dad67cb3725d
Adam Dishaw <aldishaw@gmail.com>
2016-12-02 07:47:32 -0800

    Update aggregate bins with latest ntuples. Fix missing cuts.

--- src/aggregate_bins.cxx
+++ src/aggregate_bins.cxx
@@ -21,8 +21,9 @@
 using namespace std;
 
 namespace{
-  string out_dir = "/net/cms2/cms2r0/babymaker/wspaces/agg_bins";
-  double lumi = 20.;
+  string out_dir = "/net/top/homes/ald77/ra4_stats/wspaces";
+  string syst_file = "txt/systematics/simple.txt";
+  double lumi = 36.2;
   bool do_track_veto = true;
   double met_low = 200.;
   double met_high = -1.;
@@ -30,14 +31,14 @@ namespace{
   double njets_high = -1.;
   double nbm_low = 0.5;
   double nbm_high = -1.;
-  int mglu = 1500.;
+  int mglu = 1700.;
   int mlsp = 100.;
 }
 
 int main(int argc, char *argv[]){
   GetOptions(argc, argv);
 
-  string mc = "/net/cms27/cms27r0/babymaker/2016_04_29/mc/merged_baseline/";
+  string mc = "/net/cms2/cms2r0/babymaker/babies/2016_08_10/mc/merged_mcbase_stdnj5/";
   Process ttbar{"ttbar", {
       {mc+"*TTJets*Lept*.root/tree"},
         {mc+"*TTJets*HT*.root/tree"}
@@ -60,13 +61,13 @@ int main(int argc, char *argv[]){
                                   {mc+"*_ZH_HToBB*.root/tree"},
                                     {mc+"*_ZZ_*.root/tree"},
     }, "stitch&&pass"};
-  Process data{"data", {{"/net/cms27/cms27r0/babymaker/2016_04_29/data/merged_baseline/*.root/tree"}}, "(trig[4]||trig[8])&&pass", true};
+  Process data{"data", {{"/net/cms2/cms2r0/babymaker/babies/2016_08_10/data/merged_database_stdnj5/*.root/tree"}}, "trig_ra4&&pass&&json12p9", true};
   
   Process signal{"signal", {
-      {"/net/cms27/cms27r0/babymaker/2016_04_29/mc/T1tttt/skim_baseline/*SMS-T1tttt_mGluino-"+to_string(mglu)+"_mLSP-"+to_string(mlsp)+"*.root/tree"}
-    }, "stitch&&pass", false, true};
+      {"/net/cms2/cms2r0/babymaker/babies/2016_08_10/T1tttt/merged_mcbase_standard/*SMS-T1tttt_mGluino-"+to_string(mglu)+"_mLSP-"+to_string(mlsp)+"_*.root/tree"}
+    }, "stitch", false, true};
 
-  Cut baseline("mj14>250.");
+  Cut baseline("nleps==1&&st>500&&met>200&&njets>=6&&nbm>=1&&mj14>250.");
 
   string met = "&&met>"+to_string(met_low);
   if(met_high > met_low) met += "&&met<=" + to_string(met_high);
@@ -75,15 +76,15 @@ int main(int argc, char *argv[]){
   if(nbm_high > nbm_low) njets_nbm += "&&nbm<=" + to_string(nbm_high);
   string tkveto = "&&nveto==0";
 
-  Bin r1("r1", "mt<=140.&&mj14<=400."+met, true);
-  Bin r2("r2", "mt<=140.&&mj14>400."+met+njets_nbm, true);
-  Bin r3("r3", "mt>140.&&mj14<=400."+met+tkveto, true);
-  Bin r4("r4", "mt>140.&&mj14>400."+met+njets_nbm+tkveto, true);
+  Bin r1("r1", "mt<=140.&&mj14<=400."+met+tkveto, false);
+  Bin r2("r2", "mt<=140.&&mj14>400."+met+njets_nbm+tkveto, false);
+  Bin r3("r3", "mt>140.&&mj14<=400."+met+tkveto, false);
+  Bin r4("r4", "mt>140.&&mj14>400."+met+njets_nbm+tkveto, false);
 
   set<Block> blocks = {{"all", {{r1, r2}, {r3, r4}}}};
 
   WorkspaceGenerator wg(baseline, blocks, {ttbar, other}, signal, data,
-                        "txt/systematics/simple.txt", true, 0., 1.);
+                        syst_file, true, 0., 1.);
   wg.SetRMax(10.);
   wg.SetKappaCorrected(true);
   wg.SetDoSystematics(true);
@@ -110,6 +111,7 @@ void GetOptions(int argc, char *argv[]){
   while(true){
     static struct option long_options[] = {
       {"out_dir", required_argument, 0, 0},
+      {"syst_file", required_argument, 0, 0},
       {"lumi", required_argument, 0, 0},
       {"mglu", required_argument, 0, 0},
       {"mlsp", required_argument, 0, 0},
@@ -134,6 +136,8 @@ void GetOptions(int argc, char *argv[]){
       optname = long_options[option_index].name;
       if(optname == "out_dir"){
         out_dir = optarg;
+      }else if(optname == "syst_file"){
+	syst_file = optarg;
       }else if(optname == "lumi"){
         lumi = atof(optarg);
       }else if(optname == "do_track_veto"){
